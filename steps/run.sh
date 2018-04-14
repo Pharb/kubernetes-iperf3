65636cf3dfbf01294fe2b7e21b1ab4a93525212b
Pharb <Pharb@users.noreply.github.com>
2018-04-15 00:33:48 +0200

    Wait for clients to be ready before running `kubectl exec`

--- steps/run.sh
+++ steps/run.sh
@@ -4,6 +4,10 @@ set -eu
 CLIENTS=$(kubectl get pods -l app=iperf3-client -o name | cut -d'/' -f2)
 
 for POD in ${CLIENTS}; do
+    until $(kubectl get pod ${POD} -o jsonpath='{.status.containerStatuses[0].ready}'); do
+        echo "Waiting for ${POD} to start..."
+        sleep 5
+    done
     HOST=$(kubectl get pod ${POD} -o jsonpath='{.status.hostIP}')
     kubectl exec -it ${POD} -- iperf3 -c iperf3-server -T "Client on ${HOST}" $@
     echo
